27cde7cc1763cb968a23581dea1122c5f61ad183
Morven Lewis-Everley <morven@i-lateral.com>
2024-04-18 20:14:45 +0100

    Update homing feedrates

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -2098,7 +2098,7 @@
 #endif
 
 // Homing speeds (linear=mm/min, rotational=°/min)
-#define HOMING_FEEDRATE_MM_M { (40*60), (40*60), (4*60) }
+#define HOMING_FEEDRATE_MM_M { (30*60), (30*60), (8*60) }
 
 // Validate that endstops are triggered on homing moves
 #define VALIDATE_HOMING_ENDSTOPS
