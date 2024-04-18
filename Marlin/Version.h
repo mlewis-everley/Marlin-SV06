8cb2e5576dc1e0c0ff1728bda7362c90b3c35038
Morven Lewis-Everley <morven@i-lateral.com>
2024-04-18 21:19:04 +0100

    Update icons

--- Marlin/Version.h
+++ Marlin/Version.h
@@ -28,7 +28,7 @@
 /**
  * Marlin release version identifier
  */
-#define SHORT_BUILD_VERSION "2.1.2 SV06.14"
+#define SHORT_BUILD_VERSION "2.1.2 SV06.15"
 
 /**
  * Verbose version identifier which should contain a reference to the location
